c40c69ba383f34c26cab3f48f367e18cd75d561a
Phil1910 <email@philipp-friese.de>
2014-02-19 19:02:45 +0100

    tt

--- Studiduell_Prototyp/www/js/kategorieauswaehlen.js
+++ Studiduell_Prototyp/www/js/kategorieauswaehlen.js
@@ -1,6 +1,6 @@
 function ausgewaehlteKategorie(kategorie) {
 	var kat = $(kategorie);
-	var newView = new steroids.views.WebView("html/AusgewaehlteKategorie.html");
+	var newView = new steroids.views.WebView("html/Ausgewaehltekategorie.html");
 	steroids.layers.push(newView);
 }
 
