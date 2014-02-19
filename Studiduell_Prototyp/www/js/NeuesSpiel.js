05f9f46dfafdeaf56953f7801a908953b9fd31a5
Maximilian Hlawna <milan751@hotmail.de>
2014-02-19 15:23:54 +0100

    Initiale Erstellung NeuesSpielScreen

---
+++ Studiduell_Prototyp/www/js/NeuesSpiel.js
@@ -0,0 +1,11 @@
+function init() {
+	getFriendslist();
+}
+
+function getFriendslist() {
+	/* 	1. hole die Freundesliste des Users vom Server
+		2. füge dem html Dokument für jeden user in der Freundesliste einen Eintrag "Freund" hinzu.
+	*/
+}
+
+document.addEventListener("deviceready", init, false);
--- Studiduell_Prototyp/www/js/index.js
+++ Studiduell_Prototyp/www/js/index.js
@@ -2,3 +2,8 @@ function openRundenuebersicht() {
   var RundenuebersichtView = new steroids.views.WebView("html/Rundenuebersicht.html");
   steroids.layers.push(RundenuebersichtView);
 }
+
+function openNeuesSpielScreen() {
+  var NeuesSpielView = new steroids.views.WebView("html/NeuesSpiel.html");
+  steroids.layers.push(NeuesSpielView);
+}
