26be4b8d4838ebe852b998e5f0de74f9d257070c
kevin-strobel <kevinstrobel1@gmx.de>
2014-02-21 01:18:04 +0100

    Scripts workaround git case insensitivity.

--- Studiduell_Prototyp/www/js/index.js
+++
@@ -1,9 +0,0 @@
-function openRundenuebersicht() {
-  var rundenuebersichtView = new steroids.views.WebView("html/Rundenuebersicht.html");
-  steroids.layers.push(rundenuebersichtView);
-}
-
-function openNeuesSpielScreen() {
-  var neuesSpielView = new steroids.views.WebView("html/NeuesSpiel.html");
-  steroids.layers.push(neuesSpielView);
-}
