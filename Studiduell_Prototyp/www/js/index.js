d0c22051f4156c36934382ef2a5ff152f809e46d
Phil1910 <email@philipp-friese.de>
2014-02-24 16:04:46 +0100

    s

--- Studiduell_Prototyp/www/js/index.js
+++ Studiduell_Prototyp/www/js/index.js
@@ -1,13 +1,3 @@
-function openRundenuebersicht() {
-  var rundenuebersichtView = new steroids.views.WebView("html/rundenuebersicht.html");
-  steroids.layers.push(rundenuebersichtView);
-}
-
-function openNeuesSpielScreen() {
-  var neuesSpielView = new steroids.views.WebView("html/neuesSpiel.html");
-  steroids.layers.push(neuesSpielView);
-}
-
 function openHomeScreen() {
   var HomeScreenView = new steroids.views.WebView("html/home.html");
   steroids.layers.push(HomeScreenView);
