a1c3236dcea66920bcce0d7495ec3f85e7375a9a
Maximilian Hlawna <milan751@hotmail.de>
2014-02-24 16:19:20 +0100

    initial: Spieler suchen screen

---
+++ Studiduell_Prototyp/www/js/spielerSuchen.js
@@ -0,0 +1,15 @@
+function init() {
+	
+}
+
+function test() {
+steroids.view.navigationBar.show("Titel geändert!");
+}
+
+function SpielerSuchen() {
+/* Zeige Ergebnisliste an*/
+ document.getElementById('Ergebnisliste_div').style.visibility="visible";
+  }
+
+
+document.addEventListener("deviceready", init, false);
