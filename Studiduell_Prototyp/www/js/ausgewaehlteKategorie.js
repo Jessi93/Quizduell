71425422a2e6dfb1b418704c190c927d6b83c7f8
Paul Kotte <p.kotte93@gmx.de>
2014-03-06 01:17:21 +0100

    Move Code to other Repository to separting code from documents

--- Studiduell_Prototyp/www/js/ausgewaehlteKategorie.js
+++
@@ -1,9 +0,0 @@
-function init() {
-	$("#ausgewaehlteKategorie").text("Kategorie: " + localStorage.getItem("selectedCategory"));
-}
-function starteSpiel() {
-	var newView = new steroids.views.WebView("html/frage.html");
-	steroids.layers.push(newView);
-}
-
-document.addEventListener("deviceready", init, false);
--- Studiduell_Prototyp/www/js/index.js
+++
@@ -1,5 +0,0 @@
-function openHomeScreen() {
-	var homeScreenView = new steroids.views.WebView("html/home.html");
-	steroids.layers.push(homeScreenView);
-	//steroids.layers.pop(); //nicht verwenden, da nicht zuverlässig
-}
