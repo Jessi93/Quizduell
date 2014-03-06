7e6d4128850ccd6ec6d7c64f9c724a2c4b0a4db0
Paul Kotte <p.kotte93@gmx.de>
2014-03-06 01:11:15 +0100

    spielerSuchen: add logic

--- Studiduell_Prototyp/www/js/spielerSuchen.js
+++ Studiduell_Prototyp/www/js/spielerSuchen.js
@@ -2,20 +2,41 @@ function init() {
 	
 }
 
-function test() {
-	steroids.view.navigationBar.show("Titel geändert!");
-}
 
 function spielerSuchen() {
+$.ajax( {
+		url:"http://192.168.0.105:8090/Studiduell/user/search/" + $("#search_username_input").val(),
+		type:"GET",
+		success:function(obj){addResultToList(obj);},
+		error:function(obj){alert(JSON.stringify(obj));}
+		});
+  }
 
-	alert("Spieler Suchen augerufen!");
-	/* TODO: Server nach Username fragen - Username kann wie folgt ausgelesen werden!*/
-	alert($("#search_username_input").val());
-	/* Ergebnisliste befüllen (ID = Ergebnisliste_div)*/
+function addResultToList(obj){
+	// Ergebnisliste befüllen
+	for(var i=0;i<obj.length;i++){
+	//Button zum befreunden ergänzen
+	$("#ergebnislisteErweitern").append("<li class='topcoat-list__item custom_List_item' ontouchend='createNewGame(gegnerName)'>"+obj.value[i]+"</li>");
+	}
 	
 	/* Zeige Ergebnisliste an*/
 	$("#ergebnislisteDiv").css("visibility","visible");
-  }
+}
+
+function createNewGame(gegnerName){
+$.ajax( {
+		url:"http://192.168.0.105:8090/Studiduell/game/create/with/" + gegnerName,
+		type:"POST",
+		success:function(){openHomeView();},
+		error:function(obj){alert(JSON.stringify(obj));}
+		});
+}
+
+
+function openHomeView(){
+var homeScreenView = new steroids.views.WebView("html/home.html");
+	steroids.layers.push(homeScreenView);
+}
  
 
 document.addEventListener("deviceready", init, false);
